ddf04f533f44d596464e8b75444790491bfd8c0c
namsewon1019 <swnam0729@gmail.com>
2022-01-23 17:35:24 +0900

    2022-01-23 interface 구현

---
+++ Chapter6/src/ch01/Calculator.java
@@ -0,0 +1,19 @@
+package ch01;
+
+/*abstract : 구현하지 않은 method를 포함하고 있기 때문*/
+/*implements : interface method를 구현하는 클래스 옆에다 붙임*/
+
+public abstract class Calculator implements Calc{ 
+
+	@Override
+	public int add(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	@Override
+	public int substract(int num1, int num2) {
+		return num1 - num2;
+	}
+
+	
+}
