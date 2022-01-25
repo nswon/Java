5216d96fcdc4a7248635bbeb82d1f7481924b249
namsewon1019 <swnam0729@gmail.com>
2022-01-25 14:58:58 +0900

    2022-01-25

--- Chapter1/src/ch01/Member.java
+++ Chapter1/src/ch01/Member.java
@@ -4,10 +4,12 @@ public class Member {
 
 	private int memberId; // 회원 아이디
 	private String memberName; // 회원 이름
+	private String password;
 	
 	public Member(int memberId, String memberName) { // 생성자
 		this.memberId = memberId;
 		this.memberName = memberName;
+		this.password = password;
 	}
 
 	public int getMemberId() {
@@ -25,11 +27,23 @@ public class Member {
 	public void setMemberName(String memberName) {
 		this.memberName = memberName;
 	}
+	
+	
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	@Override
-	public String toString() { //toString
-		return memberName + "회원님의 아이디는 " + memberId + "입니다.";
+	public String toString() {
+		return "Member [memberId=" + memberId + ", memberName=" + memberName + ", password=" + password + "]";
 	}
+
+	
 	
 	
 }
