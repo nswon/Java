3673c51822f506f89be0f061c8aa012f093c25e5
namsewon1019 <swnam0729@gmail.com>
2022-01-27 18:16:50 +0900

    2022-01-27

--- Chapter4/src/ch01/FileExceptionHandling.java
+++ Chapter4/src/ch01/FileExceptionHandling.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class FileExceptionHandling {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //throw 사용가능. 버린다는 뜻(?)
 
 		FileInputStream fis = null;
 		
@@ -16,7 +16,8 @@ public class FileExceptionHandling {
 			System.out.println("log.info : read ok");
 			
 		} catch (FileNotFoundException e) {
-			System.out.println(e.toString()); //어디서 error가 났늕 console에 찍힘
+			System.out.println(e.toString()); 
+			//어디서 error가 났늕 console에 찍힘
 		} finally { //try가 불리면 finally도 항상 호출이 된다. try마지막에 return;을 찍어도 log.info: finally는 출력된다.
 			if(fis != null) {
 				try {
