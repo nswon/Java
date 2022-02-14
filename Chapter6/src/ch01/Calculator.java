2653138b98415557202b29ceb7f16a767f409397
namsewon1019 <swnam0729@gmail.com>
2022-02-14 19:12:02 +0900

    2022-02-14

--- Chapter6/src/ch01/Calculator.java
+++ Chapter6/src/ch01/Calculator.java
@@ -1,7 +1,7 @@
 package ch01;
 
-/*abstract : 구현하지 않은 method를 포함하고 있기 때문*/
-/*implements : interface method를 구현하는 클래스 옆에다 붙임*/
+/*abstract : 구현하지 않은 method를 포함하고 있기 때문
+implements : interface method를 구현하는 클래스 옆에다 붙임*/
 
 public abstract class Calculator implements Calc{ 
 
